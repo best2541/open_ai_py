bf4b5a7faf8c0867d70fddca5fe713caca6d2e1c
best2541 <60605549+best2541@users.noreply.github.com>
2024-08-13 10:05:46 +0700

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, TIMESTAMP
 from utilites.database import Base
 
 class User(Base):
     __tablename__ = "USERS"
 
-    username = Column(String(100), primary_key=True)
+    user_id = Column(String(255), primary_key=True)
+    username = Column(String(100))
     password = Column(String(100))
     age = Column(Integer)
     country = Column(Integer)
@@ -13,12 +14,19 @@ class Activities(Base):
     __tablename__ = "ACTIVITIES"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(100), nullable=False)
-    name = Column(String(100), nullable=False)
-    detail = Column(String(255))
+    user_id = Column(String(255), nullable=False)
+    topic = Column(String(100), nullable=False)
+    start_date_time = Column(TIMESTAMP)
 
 class Countries(Base):
     __tablename__ = "COUNTRY"
 
     id = Column(Integer, primary_key=True)
     country_name = Column(String(100), nullable=False)
+class Logs(Base):
+    __tablename__ = "Logs"
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(255))
+    detail = Column(String(255))
+    create_date = Column(TIMESTAMP)
