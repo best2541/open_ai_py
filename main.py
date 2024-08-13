eeeaeefb1630c5e6e310a81a778355dae87b6c24
best2541 <60605549+best2541@users.noreply.github.com>
2024-08-13 11:09:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from routes.chat import router
 from routes.users import router as users_router
 from routes.setting import router as setting_router
+from typing import Dict, Any
+from controllers import crud
+from sqlalchemy.orm import Session
+from utilites.database import SessionLocal
 
 # Create all tables
 # from models import Base
 # Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
 app = FastAPI()
 
 load_dotenv()
@@ -26,6 +37,17 @@ app.include_router(users_router, prefix="/users")
 app.include_router(router, prefix='/chat')
 app.include_router(setting_router, prefix='/setting')
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+@app.get('/webhook')
+def test():
+    return 'ok'
+@app.post('/webhook')
+def oh(db: Session = Depends(get_db)):
+    result = crud.get_log(db=db, user_id='random')
+    return result
+@app.get('/')
+def getStatus():
+    return "OK"
+@app.post('/')
+def post(item:Dict[str,Any], db: Session = Depends(get_db)):
+    result = crud.sendLine(db=db, item=item)
+    return result
